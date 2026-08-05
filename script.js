593550f6424207da391640fc60978ef7b556d254
Oscar Pato <oscar.andres.0815@gmail.com>
2026-08-05 16:05:20 -0600

    FrontEnd

---
+++ script.js
@@ -0,0 +1,5 @@
+function changeAboutTitle() {
+    document.getElementById("aboutTitle").textContent = "JavaScript Changed This!";
+    document.getElementById("message").textContent = 
+        "The about page is now using JavaScript.";
+}
